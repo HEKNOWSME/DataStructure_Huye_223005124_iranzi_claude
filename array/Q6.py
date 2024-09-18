d0b3280c1c27e8e7ae035d32d64076b78051c2d8
HEKNOWSME <iranziclaude@hotmail.com>
2024-09-18 22:13:46 +0200

    a 3x3 tic-tac-toe board to check winner

---
+++ array/Q6.py
@@ -0,0 +1,16 @@
+board = [
+     ["x", "o", "x"], # row 1
+     ["o", "x", "o"], # row 2
+     ["o", "x", "o"], # row 3
+]
+def check_winner(board): 
+     for row in board:
+          if row[0] == row[1] == row[2]:
+               return f"the winner is {row[0]}"
+     for col in range(3):
+          if board[0][col] == board[1][col] == board[2][col]:
+               return f"the winner is {board[0][col]}"
+     if board[0][0] == board[1][1] == board[2][2]:
+          return f"the winner is {board[0][0]}"
+     return "Toe"
+print(check_winner(board))
