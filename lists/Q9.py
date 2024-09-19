ccad178a1e11b8f6991285f7759a13f7ff494d0e
HEKNOWSME <iranziclaude@hotmail.com>
2024-09-19 22:07:39 +0200

    modify challenge 9

--- lists/Q9.py
+++ lists/Q9.py
@@ -1,10 +1,12 @@
 stock_product = [
-       ['product 1', 500, 18], 
-       ['product 2', 400, 0], 
-       ['product 3', 600, 18],
-       ['product 4', 300, 0],
+       ['Meat', 500, 18], 
+       ['Milk', 400, 0], 
+       ['Eggs', 600, 18],
+       ['Potatoes', 300, 0],
        ]
 out_of_stock = [product for product in stock_product if product[2] == 0]
 out_of_stock_product_names = [product[0] for product in stock_product if product[2] == 0]
+available_products = [product for product in stock_product if product[2] > 0]
 print(out_of_stock)
-print(out_of_stock_product_names)
+print( "products out of the stock are", out_of_stock_product_names)
+print("available products are", available_products)
