e26ebd24a10386f322f767f21a778cb75ed29791
HEKNOWSME <iranziclaude@hotmail.com>
2024-09-19 21:22:54 +0200

    Create a 2D array that represents an RGB image (3 channels for Red, Green, Blue), and invert the colors.

--- lists/Q7.py
+++ lists/Q7.py
@@ -1,16 +1,3 @@
-rgb_image = [
-       [0, 0, 255], # Red 
-       [0, 255, 0 ], # green
-       [0, 0, 255]   # blue
-       ] 
-def rgb_image_invert_colors(colors):
-       for color in colors:
-              color[0] = 255 -color[0]
-              color[1] = 255 -color[1]
-              color[2] = 255 -color[2]
-       return colors
-print(rgb_image_invert_colors(rgb_image))
-
 sales = [
     [100, 150, 200, 250, 300, 350, 400], 
     [120, 130, 140, 160, 180, 200, 220],  
